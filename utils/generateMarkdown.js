35a7498c7a01983a6f3072b28a7a47e4600a0fec
Sarah Hinton <smcfarland2@gmail.com>
2021-05-02 21:53:42 +1000

    writing license functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,27 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (data.license !== "None") {
+    return `https://img.shields.io/badge/License-${data.license}-lightgreen`
+  } else "";
+}
+
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (data.license !== "None") {
+    // write link to License.txt file
+  } else "";
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// function renderLicenseSection(license) {
+//   renderLicenseBadge()
+//   renderLicenseLink()
+// }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -35,7 +48,7 @@ function generateMarkdown(data) {
   ${renderLicenseSection(data.license)}
 
 
- ## Description
+  ## Description
   ${data.description}
   
   
@@ -47,8 +60,6 @@ function generateMarkdown(data) {
  ${data.usage}
  
   
-  
-
   ## Contributing
   ${data.contributions}
  
