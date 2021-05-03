ba98a95e0af909fcb55f1c62859cf6e30d9752bb
Sarah Hinton <smcfarland2@gmail.com>
2021-05-03 14:51:41 +1000

    working badges functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -16,12 +16,29 @@ function renderLicenseLink(license) {
   } else "";
 }
 
+
+var MIT = `The MIT license gives users express permission to reuse code for any purpose, 
+sometimes even if code is part of proprietary software. As long as users include the original 
+copy of the MIT license in their distribution, they can make any changes or modifications to the 
+code to suit their own needs.`;
+
+var apache = `Apache License 2.0 allows users of the software to distribute, modify, or otherwise use 
+software for any purpose, as long as the user complies with the license terms. The terms state that 
+users can’t remove existing copyright, patent, trademarks and attribution notices.`;
+
+var GPL = `You may copy, distribute and modify the software as long as you track changes/dates in source files. 
+Any modifications to or software including (via compiler) GPL-licensed 
+code must also be made available under the GPL along with build & install instructions.`;
+
+var BSD = `The BSD 3-clause license allows you almost unlimited freedom with the software so 
+long as you include the BSD copyright and license notice in it (found in Fulltext). `;
+
+
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-// function renderLicenseSection(license) {
-//   renderLicenseBadge()
-//   renderLicenseLink()
-// }
+function renderLicenseSection(license) {
+  
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
